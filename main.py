90e04d0da746f4821faa93d640a146a90e70b309
SLEEPWELL <66638927+Lukajekul@users.noreply.github.com>
2025-04-11 06:30:18 +0000

    main in zacetek logina

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-flask
-zivjo to je commit od Leona :)
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    ime = request.args.get("ime")
+    geslo = request.args.get("geslo")
+
+app.run(debug=True)
